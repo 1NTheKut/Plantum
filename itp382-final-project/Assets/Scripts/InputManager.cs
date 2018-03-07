7b9437f29c5140bf4692ad6d7f0a5f4265bfac4f
Carolyn DiLoreto <cdiloret@usc.edu>
2018-03-07 01:22:09 -0800

    Feature: Player plants tree at their location

--- itp382-final-project/Assets/Scripts/InputManager.cs
+++ itp382-final-project/Assets/Scripts/InputManager.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class InputManager : MonoBehaviour {
 
 	public GameObject treePreFab;
-	public GameObject world;
-	Vector3 centerOfWorld;
-	Vector3 pos;
-	Vector3 normal;
-
+	GameObject world;
+	GameObject player;
+	bool isFreePos;
 
 	// Use this for initialization
 	void Start () {
@@ -17,34 +15,51 @@ public class InputManager : MonoBehaviour {
 		if (!supportsMultiTouch) {
 			print ("Multitouch not supported");
 		}
-		centerOfWorld = world.transform.position;
+		isFreePos = true;
+		world = GameObject.Find ("Planet");
+		player = GameObject.Find("Player");
 	}
 
 	// Update is called once per frame
 	void Update () {
 		int nbTouches = Input.touchCount;
 
+
 		if (nbTouches > 0) {
 			for (int i = 0; i < nbTouches; i++) {
 				Touch touch = Input.GetTouch (i);
 
 				if (touch.phase == TouchPhase.Began) {
-					GameObject tree = Instantiate<GameObject> (treePreFab);
-					tree.transform.position = touch.position;
-					//tree.transform.position.y = world.transform.position.y + world.
+					PlantTree ();
 				} 
 
 			}
 		} else if (Input.GetMouseButtonDown (0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast (ray, out hit)) {
-				Vector3 spawnPosition = Vector3.Normalize (hit.point) * ((world.transform.localScale.x / 2) + treePreFab.transform.localScale.y * 0.5f) + world.transform.position;
-				Quaternion spawnRotation = Quaternion.identity;
-				GameObject newCharacter = Instantiate(treePreFab, spawnPosition, spawnRotation) as GameObject;
-				newCharacter.transform.LookAt(world.transform);
-				newCharacter.transform.Rotate(-90, 0, 0);
+			PlantTree ();
+
+		}
+		isFreePos = true;
+	}
+
+	void PlantTree() {
+		foreach(GameObject plantedTree in GameObject.FindGameObjectsWithTag("tree"))
+		{
+			//Debug.Log ("PlayerPos = " + player.transform.position.x + ", treePos = " + plantedTree.transform.position.x);
+			if(Mathf.Round(player.transform.position.x) == Mathf.Round(plantedTree.transform.position.x))
+			{
+				//Debug.Log ("Found tree");
+				isFreePos = false;
 			}
+		}
+		if (isFreePos) {
+			//If no tree there already, create another tree
+			Vector3 spawnPos = player.transform.position;
+			Quaternion spawnRotation = Quaternion.identity;
+			spawnPos.z += 1;
+			spawnPos.y -= .5f;
+			GameObject newCharacter = Instantiate(treePreFab, spawnPos, spawnRotation) as GameObject;
+			newCharacter.transform.LookAt(world.transform);
+			newCharacter.transform.Rotate(-90, 0, 0);
 		}
 	}
 }
