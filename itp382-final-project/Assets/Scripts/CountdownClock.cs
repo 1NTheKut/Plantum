314bcdbc0f67b5f3257fc4cfd2c168b93fba1573
Roth5child <spearlma@usc.edu>
2018-03-20 20:44:15 -0700

    Finished timer implementation

--- itp382-final-project/Assets/Scripts/CountdownClock.cs
+++ itp382-final-project/Assets/Scripts/CountdownClock.cs
@@ -2,18 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CountdownClock : MonoBehaviour
 {
-
-	public float timeLeft = 60.0f;
-
+	public float timeLeft = 60f;
 	public Text timerText;
 
 	// Use this for initialization
 	void Start ()
 	{
-		//find timer text
 		GameObject timerGO = GameObject.Find ("Countdown");
 		timerText = timerGO.GetComponent<Text> ();
 	}
@@ -21,12 +19,13 @@ public class CountdownClock : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		timeLeft -= Time.deltaTime;
-		timerText.text = "Time Left: " + timeLeft.ToString ();
 
+		timeLeft -= Time.deltaTime;
+		timerText.text = "Time Left: " + timeLeft.ToString("f2");
 		if (timeLeft <= 0) {
-			Application.LoadLevel("WinScene");
+			SceneManager.LoadScene("LoseScene");
 		}
 
 	}
+		
 }
