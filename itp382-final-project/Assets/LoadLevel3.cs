6f8bf227bbc819c2e2065e445dbaac8f69749bcf
Roth5child <spearlma@usc.edu>
2018-04-10 18:41:03 -0700

    Fixed logic to transition between levels and start screen

---
+++ itp382-final-project/Assets/LoadLevel3.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using UnityEngine;
+
+//make each level on the same scene and have everything data
+//driven so you only change the data of each scene
+
+//show represenation for the different plants and how they differ
+public class LoadLevel3 : MonoBehaviour {
+
+	public Button nextLevelButton;
+
+	// Use this for initialization
+	void Start () {
+		Button loadButton = nextLevelButton.GetComponent<Button> ();
+		loadButton.onClick.AddListener (LoadLevel);
+	}
+
+	public void LoadLevel(){
+		SceneManager.LoadScene ("Level3");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
